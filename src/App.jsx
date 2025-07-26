0fe0540b0396a02f3c71a756b6807da4d60a1826
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-07-26 17:43:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/UI/Navbar'
-
-import "./components/lightswind.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
+  const router= createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar/></>
+    }
+  ])
 
   return (
     <>
-     <Navbar/>
-     <div className="sec1 bg-amber-600 w-full h-screen">section 1</div>
-     <div className="sec1 bg-violet-600 w-full h-screen"> section2</div>
+     <RouterProvider router={router}/>
     </>
   )
 }
